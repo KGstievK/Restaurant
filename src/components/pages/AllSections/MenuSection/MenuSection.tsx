e1eb95ae949d19c096b86f8b70020ee395191b60
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-10-21 13:06:31 +0600

    update

--- src/components/pages/AllSections/MenuSection/MenuSection.tsx
+++ src/components/pages/AllSections/MenuSection/MenuSection.tsx
@@ -1,3 +1,4 @@
+import MenuTab from '@/ui/menuTab/MenuTab'
 import scss from './MenuSection.module.scss'
 
 const MenuSection = () => {
@@ -6,10 +7,10 @@ const MenuSection = () => {
       <div className="container">
         <div className={scss.content}>
           <h1>MenuSection</h1>
+          <MenuTab/>
         </div>
       </div>
     </section>
   )
 }
-
 export default MenuSection
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,7 @@
 import VisitRestaurant from "./AllSections/fifthSection/VisitRestaurant";
 import Delicious from "./AllSections/firstSection/Delicious";
 import Interior from "./AllSections/fourthSection/Interior";
-import MenuSection from "./AllSections/MenuSection/MenuSection/MenuSection";
+import MenuSection from "./AllSections/MenuSection/MenuSection";
 import About from "./AllSections/secondSection/About";
 import BestSellers from "./AllSections/thirdSection/BestSellers";
 
