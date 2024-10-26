b402462ab40b2c4fb0a77ed7322ff485aad6250d
ayat kalybekova <kalybekAyat@outlook.com>
2024-10-26 15:10:26 +0300

    a

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,28 +1,8 @@
-<<<<<<< HEAD
-import VisitRestaurant from './AllSections/fifthSection/VisitRestaurant'
-import Delicious from './AllSections/firstSection/Delicious'
-import Interior from './AllSections/fourthSection/Interior'
-import Menu from './AllSections/menuSection/menusection'
-import About from './AllSections/secondSection/About'
-import BestSellers from './AllSections/thirdSection/BestSellers'
-
-const Home = () => {
-	return (
-		<div>
-			<Delicious />
-			<About />
-			<BestSellers />
-			<Menu />
-			<Interior />
-			<VisitRestaurant />
-		</div>
-	)
-}
-=======
 import VisitRestaurant from "./AllSections/fifthSection/VisitRestaurant";
 import Delicious from "./AllSections/firstSection/Delicious";
 import Interior from "./AllSections/fourthSection/Interior";
-import MenuSection from "./AllSections/MenuSection/MenuSection";
+import MenuSection from "./AllSections/MenuSection/menusection";
+// import Menu from './AllSections/menuSection/menusection'
 import About from "./AllSections/secondSection/About";
 import BestSellers from "./AllSections/thirdSection/BestSellers";
 
@@ -32,12 +12,9 @@ const Home = () => {
       <Delicious />
       <About />
       <BestSellers />
-      <MenuSection/>
+      <MenuSection />
       <Interior />
       <VisitRestaurant />
     </div>
   );
 };
->>>>>>> 2814f2d9de884db401fd843c14151d371b69e630
-
-export default Home
